76b5dceb29b991a178200010d83894c61b07da6d
Xinqi Li <lxinqi@vmware.com>
2022-02-03 16:28:43 -0800

    run in windows cluster

--- build_k8s_test_binary.sh
+++ build_k8s_test_binary.sh
@@ -1,8 +1,8 @@
-rm -rf kubernetes
-rm ./e2e.test
-git clone https://github.com/kubernetes/kubernetes.git
+# rm -rf kubernetes
+# rm ./e2e.test
+# git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
-KUBE_BUILD_PLATFORMS=linux/amd64 make WHAT="test/e2e/e2e.test"
-cp ./_output/bin/e2e.test ../e2e.test
+./build/run.sh make WHAT="test/e2e/e2e.test" KUBE_BUILD_PLATFORMS=linux/amd64
+cp ./_output/dockerized/bin/linux/amd64/e2e.test ../e2e.test
 cd ..
 # rm -rf kubernetes

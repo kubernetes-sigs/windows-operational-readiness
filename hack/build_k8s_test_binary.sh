0b6616de548c671b7e27a65972585ca8aa561a74
Xinqi Li <lxinqi@vmware.com>
2022-05-12 14:29:57 -0700

    add core.Network tests

--- hack/build_k8s_test_binary.sh
+++ hack/build_k8s_test_binary.sh
@@ -1,13 +1,17 @@
 # rm -rf kubernetes
 # rm ./e2e.test
-# git clone https://github.com/kubernetes/kubernetes.git
+git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
-make WHAT="test/e2e/e2e.test"
-mkdir -p ../e2e_test_binary/darwin/
-cp ./_output/bin/e2e.test ../e2e_test_binary/darwin/e2e.test
+# make WHAT="test/e2e/e2e.test"
+# mkdir -p ../e2e_test_binary/darwin/
+# cp ./_output/bin/e2e.test ../e2e_test_binary/darwin/e2e.test
 
-./build/run.sh make WHAT="test/e2e/e2e.test" KUBE_BUILD_PLATFORMS=linux/amd64
+make WHAT="test/e2e/e2e.test"
 mkdir -p ../e2e_test_binary/linux/
-cp ./_output/dockerized/bin/linux/amd64/e2e.test ../e2e_test_binary/linux/e2e.test
+cp ./_output/bin/e2e.test ../e2e.test
+
+# ./build/run.sh make WHAT="test/e2e/e2e.test" KUBE_BUILD_PLATFORMS=linux/amd64
+# mkdir -p ../e2e_test_binary/linux/
+# cp ./_output/dockerized/bin/linux/amd64/e2e.test ../e2e_test_binary/linux/e2e.test
 cd ..
 # rm -rf kubernetes

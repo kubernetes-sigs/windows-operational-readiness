e1389d7ef2e754a8ec5cfd25e6bdfb14beb2e9dc
op-readiness-team <jayunit100.github@gmail.com>
2023-09-29 09:54:59 +0000

    WIP, tf dev cluster for op-red, not working yet....
    >
    Co-authored-by: arnaud meukam <ameukam@gmail.com>

--- terraform/jumper/data.tf
+++ terraform/jumper/data.tf
@@ -43,11 +43,4 @@ data "aws_ami" "ubuntu_2204" {
   }
 }
 
-locals {
-  name   = "${basename(path.cwd)}"
-  region = "eu-east-2"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-}
 
